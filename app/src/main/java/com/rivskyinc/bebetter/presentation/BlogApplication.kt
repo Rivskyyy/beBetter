8c1a7422aaba441223ba58a1572bb16711ed73c5
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-01-19 06:18:55 +0200

    BlogApplication.kt modified

--- app/src/main/java/com/rivskyinc/bebetter/presentation/BlogApplication.kt
+++ app/src/main/java/com/rivskyinc/bebetter/presentation/BlogApplication.kt
@@ -1,5 +1,11 @@
 package com.rivskyinc.bebetter.presentation
 
-class BlogApplication {
+import android.app.Application
+import com.rivskyinc.bebetter.di.DaggerApplicationComponent
 
+class BlogApplication : Application() {
+
+    val component by lazy {
+        DaggerApplicationComponent.factory().create(this)
+    }
 }
