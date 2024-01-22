8059565e44accee70f7cfe2bade1d7d452fbcd7d
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-01-22 08:20:01 +0200

    FirebaseApi fixed Blog to List<Blog>

--- app/src/main/java/com/rivskyinc/bebetter/domain/FirebaseApi.kt
+++ app/src/main/java/com/rivskyinc/bebetter/domain/FirebaseApi.kt
@@ -4,6 +4,6 @@ import com.rivskyinc.bebetter.domain.entities.Blog
 import com.rivskyinc.bebetter.domain.entities.Quotes
 
 interface FirebaseApi {
-    suspend fun getPosts(): Blog
+    suspend fun getPosts(): List<Blog>
     suspend fun getQuote(): Quotes
 }
