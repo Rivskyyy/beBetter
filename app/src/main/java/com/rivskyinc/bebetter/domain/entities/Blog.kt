f184f16f0c78b2fce950c86deb606ea14edac37f
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2024-01-23 05:36:05 +0200

    added id for Blog

--- app/src/main/java/com/rivskyinc/bebetter/domain/entities/Blog.kt
+++ app/src/main/java/com/rivskyinc/bebetter/domain/entities/Blog.kt
@@ -6,21 +6,31 @@ package com.rivskyinc.bebetter.domain.entities
      var text: String? = null
      var thumbnail: String? = null
      var title: String? = null
+     var id : String? = null
 
      constructor() {
 
      }
 
-     constructor(category: String?, text: String?, thumbnail: String?, title: String?) {
+     constructor(category: String?,
+                 text: String?,
+                 thumbnail: String?,
+                 title: String?,
+                 id : String?) {
          this.category = category
          this.text = text
          this.thumbnail = thumbnail
          this.title = title
+         this.id = id
 //    this.timestamp = timestamp
      }
 
      override fun toString(): String {
-         return "Blog(category='$category', text='$text', thumbnail='$thumbnail, title='$title')"
+         return "Blog(category='$category'," +
+                 " text='$text', " +
+                 "thumbnail='$thumbnail," +
+                 " title='$title', " +
+                 "id='$id')"
      }
  }
 
